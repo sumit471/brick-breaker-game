3aa3b4ae2bedc1fbeaebcc7c66a79ab61958fba5
= <sumitpal90123@gmail.com>
2021-08-19 10:24:00 +0530

    added paddle and ball to the canvas

--- script.js
+++ script.js
@@ -1,211 +1,230 @@
-const container = document.querySelector('.container');
-        let conDim = container.getBoundingClientRect();
-        const gameover = document.createElement('div');
-        gameover.textContent = "Start Game";
-        gameover.style.position = "absolute";
-        gameover.style.color = "white";
-        gameover.style.lineHeight = "60px";
-        gameover.style.height = "250px";
-        gameover.style.textAlign = "center";
-        gameover.style.fontSize = "3em";
-        gameover.style.textTransform = "uppercase";
-        gameover.style.backgroundColor = "red";
-        gameover.style.width = "100%";
-        gameover.addEventListener('click', startGame);
-        container.appendChild(gameover);
-        const ball = document.createElement('div');
-        ball.style.position = "absolute";
-        ball.style.width = "20px";
-        ball.style.height = "20px";
-        ball.style.backgroundColor = "white";
-        ball.style.borderRadius = "25px";
-        ball.style.backgroundImage = "url('ball.png')";
-        ball.style.backgroundSize = "20px 20px";
-        ball.style.top = "70%";
-        ball.style.left = "50%";
-        ball.style.display = "none";
-        container.appendChild(ball);
-        const paddle = document.createElement('div');
-        paddle.style.position = "absolute";
-        paddle.style.backgroundColor = "white";
-        paddle.style.height = "20px";
-        paddle.style.width = "100px";
-        paddle.style.borderRadius = "25px";
-        paddle.style.bottom = "30px";
-        paddle.style.left = "50%";
-        container.appendChild(paddle);
-        document.addEventListener('keydown', function (e) {
-            //console.log(e.keyCode);
-            if (e.keyCode === 37) paddle.left = true;
-            if (e.keyCode === 39) paddle.right = true;
-            if (e.keyCode === 38 && !player.inPlay) player.inPlay = true;
-        })
-        document.addEventListener('keyup', function (e) {
-            //console.log(e.keyCode);
-            if (e.keyCode === 37) paddle.left = false;
-            if (e.keyCode === 39) paddle.right = false;
-        })
-        const player = {
-            gameover: true
-        };
+const cnv = document.getElementById("breakout");
+const ctx = cnv.getContext("2d");
 
-        function startGame() {
-            if (player.gameover) {
-                player.gameover = false;
-                gameover.style.display = "none";
-                player.score = 0;
-                player.lives = 1;
-                player.inPlay = false;
-                ball.style.display = "block";
-                ball.style.left = paddle.offsetLeft + 50 + "px";
-                ball.style.top = paddle.offsetTop - 30 + "px";
-                player.ballDir = [2, -5];
-                player.num = 80;
-                setupBricks(player.num);
-                scoreUpdater();
-                player.ani = window.requestAnimationFrame(update);
-            }
-        }
 
-        function setupBricks(num) {
-            let row = {
-                x: ((conDim.width % 100) / 2)
-                , y: 50
-            }
-            let skip = false;
-            for (let x = 0; x < num; x++) {
-                //console.log(row);
-                if (row.x > (conDim.width - 100)) {
-                    row.y += 50;
-                    if (row.y > (conDim.height / 2)) {
-                        skip = true;
-                    }
-                    row.x = ((conDim.width % 100) / 2);
-                }
-                row.count = x;
-                if (!skip) {
-                    createBrick(row);
-                }
-                row.x += 100;
-            }
-        }
+//Object for used images
+var BG_IMG = new Image();
+BG_IMG.src = "img/bg.png";
 
-        function createBrick(pos) {
-            const div = document.createElement('div');
-            div.setAttribute('class', 'brick');
-            div.style.backgroundColor = rColor();
-            div.textContent = pos.count + 1;
-            div.style.left = pos.x + 'px';
-            div.style.top = pos.y + 'px';
-            container.appendChild(div);
-        }
 
-        function isCollide(a, b) {
-            let aRect = a.getBoundingClientRect();
-            let bRect = b.getBoundingClientRect();
-            return !((aRect.right < bRect.left) || (aRect.left > bRect.right) || (aRect.bottom < bRect.top) || (aRect.top > bRect.bottom));
-        }
 
-        function rColor() {
-            return '#' + Math.random().toString(16).substr(-6);
-        }
 
-        function scoreUpdater() {
-            document.querySelector('.score').textContent = player.score;
-            document.querySelector('.lives').textContent = player.lives;
-        }
 
-        function update() {
-            if (!player.gameover) {
-                let pCurrent = paddle.offsetLeft;
-                if (paddle.left && pCurrent > 0) {
-                    pCurrent -= 5;
-                }
-                if (paddle.right && (pCurrent < (conDim.width - paddle.offsetWidth))) {
-                    pCurrent += 5;
-                }
-                paddle.style.left = pCurrent + 'px';
-                if (!player.inPlay) {
-                    waitingOnPaddle();
-                }
-                else {
-                    moveBall();
-                }
-                player.ani = window.requestAnimationFrame(update);
-            }
-        }
+//Used Constant
+let PADDLE_WIDTH=150;
+let PADDLE_HEIGHT=20;
+let PADDLE_MARGIN_BOTTOM=50
+let BALL_RADIUS=10;
+let LIFE = 3;
+let SCORE_UNIT = 10;
+let SCORE=0;
+let LEVEL = 1;
+const MAX_LEVEL = 3;
+let GAME_OVER = false;
+let leftArrow = false;
+let rightArrow = false;
+let upArrow = false;
 
-        function waitingOnPaddle() {
-            ball.style.top = (paddle.offsetTop - 22) + 'px';
-            ball.style.left = (paddle.offsetLeft + 40) + 'px';
-        }
+//paddle object
+let paddle = {
+    x: cnv.width/2 - (PADDLE_WIDTH/2),
+    y: cnv.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    dx:5
+}
 
-        function fallOff() {
-            player.lives--;
-            if (player.lives < 0) {
-                endGame();
-                player.lives = 0;
-            }
-            scoreUpdater();
-            stopper();
-        }
+//draw the paddle
+function drawPaddle()
+{
+    ctx.fillStyle = "#2e3548";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    
+    ctx.strokeStyle = "#ffcd05";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
 
-        function endGame() {
-            gameover.style.display = "block";
-            gameover.innerHTML = "Game Over<br>Your score " + player.score;
-            player.gameover = true;
-            ball.style.display = "none";
-            let tempBricks = document.querySelectorAll('.brick');
-            for (let tBrick of tempBricks) {
-                tBrick.parentNode.removeChild(tBrick);
-            }
-            window.cancelAnimationFrame(player.ani);
-        }
+// Control Paddle
+document.addEventListener("keydown", function(event){
+    if(event.keyCode==37)
+       leftArrow=true;
+    else if(event.keyCode==39)
+       rightArrow=true;
+    else if(event.keyCode==38){// make ball move after clicking the up button
+       upArrow=true;
+       ball.speed = 5;
+       ball.dx = 3*(Math.random()*2-1);
+       ball.dy = -3;
+    }    
+});
 
-        function stopper() {
-            player.inPlay = false;
-            player.ballDir[0, -5];
-            waitingOnPaddle();
-            window.cancelAnimationFrame(player.ani);
-        }
+document.addEventListener("keyup", function(event){
+    if(event.keyCode==37)
+       leftArrow=false;
+    else if(event.keyCode==39)
+       rightArrow=false;    
+});
 
-        function moveBall() {
-            let posBall = {
-                x: ball.offsetLeft
-                , y: ball.offsetTop
-            }
-            if (posBall.y > (conDim.height - 20) || posBall.y < 0) {
-                if (posBall.y > (conDim.height - 20)) {
-                    fallOff();
-                }
-                else {
-                    player.ballDir[1] *= -1;
-                }
-            }
-            if (posBall.x > (conDim.width - 20) || posBall.x < 0) {
-                player.ballDir[0] *= -1;
-            }
-            if (isCollide(paddle, ball)) {
-                let temp = ((posBall.x - paddle.offsetLeft) - (paddle.offsetWidth / 2)) / 10;
-                console.log('hit');
-                player.ballDir[0] = temp;
-                player.ballDir[1] *= -1;
-            };
-            let bricks = document.querySelectorAll('.brick');
-            if (bricks.length == 0 && !player.gameover) {
-                stopper();
-                setupBricks(player.num);
-            }
-            for (let tBrick of bricks) {
-                if (isCollide(tBrick, ball)) {
-                    player.ballDir[1] *= -1;
-                    tBrick.parentNode.removeChild(tBrick);
-                    player.score++;
-                    scoreUpdater();
-                }
-            }
-            posBall.y += player.ballDir[1];
-            posBall.x += player.ballDir[0];
-            ball.style.top = posBall.y + 'px';
-            ball.style.left = posBall.x + 'px';
-        }
+
+//Move Padlle
+function movePaddle()
+{
+    if(rightArrow && paddle.x + paddle.width < cnv.width)
+       paddle.x += paddle.dx;    
+    else if(leftArrow && paddle.x > 0)
+       paddle.x -= paddle.dx;   
+}
+
+// create the ball object
+let ball = {
+    x: cnv.width/2,
+    y: paddle.y - BALL_RADIUS,
+    rad: BALL_RADIUS,
+    speed : 0,
+    dx : 0,
+    dy : 0
+}
+
+//Draw ball
+function drawBall()
+{
+    ctx.beginPath();
+
+    ctx.arc(ball.x, ball.y, ball.rad, 0, Math.PI*2);
+
+    ctx.fillStyle = "#ffcd05";
+    ctx.fill();
+
+    ctx.strokeStyle = "#2e3548";
+    ctx.stroke();
+
+    ctx.closePath();
+}
+
+// move ball
+function moveBall()
+{
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+
+// BALL AND WALL COLLISION DETECTION
+function ballWallCollision()
+{
+    //Collision with right wall and left wall
+    if(ball.x + ball.rad > cnv.width || ball.x - ball.rad < 0)
+    {
+        ball.dx = -ball.dx;
+        WALL_HIT.play();
+    }
+
+    //Collision with upper wall
+    if(ball.y - ball.rad < 0){
+        ball.dy = -ball.dy; 
+        WALL_HIT.play();       
+    }
+
+    //Collison with base
+    if(ball.y + ball.rad > cnv.height){
+        LIFE--;
+        LOSE.play();        
+        resetBall();
+        resetPaddle();
+        upArrow=false;
+        stayBall();
+    }
+}
+
+// reset the ball it's initial position as it collides with base wall
+function resetBall()
+{
+    ball.x= cnv.width/2,
+    ball.y= paddle.y - BALL_RADIUS,
+    ball.dx = 5, 
+    ball.dy = -5
+}
+
+
+// reset the paddle at it's initial position as it ball collides with base wall
+function resetPaddle()
+{
+    paddle.x= cnv.width/2 - (PADDLE_WIDTH/2),
+    paddle.y= cnv.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    paddle.width= PADDLE_WIDTH,
+    paddle.height= PADDLE_HEIGHT,
+    paddle.dx=5
+}
+
+// make ball stay at initian postion
+function stayBall()
+{
+    ball.speed = 0;
+    ball.dx = 0;
+    ball.dy = 0;
+}
+
+
+// Ball paddle collision detection
+function ballPaddleCollision()
+{
+    if(ball.y + ball.rad > paddle.y &&  ball.x > paddle.x && ball.x < paddle.x + paddle.width )
+    {
+         PADDLE_HIT.play();
+
+        // + if ball collides right to mid point and - if ball collides left to mid point of paddle 
+          let colliPoint = ball.x - (paddle.x + paddle.width/2); 
+        
+        // Normalize the point
+          colliPoint = colliPoint/(paddle.width/2);
+
+        // Angle at which ball return from paddle after collision
+          let angle = colliPoint * (Math.PI/3);
+          ball.dx = ball.speed * Math.sin(angle);
+          ball.dy = -ball.speed * Math.cos(angle);
+    }
+}
+
+
+
+// To draw diffrent structure on canvas
+function draw()
+{
+    drawPaddle();  
+    drawBall();
+    drawBricks();
+
+    // SHOW SCORE
+    showGameStats(SCORE, 50, 35, SCORE_IMG, 10, 5);
+    // SHOW LIVES
+    showGameStats(LIFE, cnv.width - 25, 35, LIFE_IMG, cnv.width-70, 5); 
+    // SHOW LEVEL
+    showGameStats(LEVEL+"/3", cnv.width/2, 40, LEVEL_IMG, cnv.width/2 - 40, 5);
+}
+
+
+//update the canvas as button clicked
+function update()
+{
+    movePaddle();
+    moveBall();
+    ballWallCollision();
+    ballPaddleCollision();
+    ballBrickCollision();
+    gameOver();
+    levelUp()
+}
+
+function loop()
+{
+    // ctx.clearRect(0,0,cnv.width,cnv.height);
+    ctx.drawImage(BG_IMG,0,0,cnv.width,cnv.height);
+    draw()
+    update()
+    
+
+    if(! GAME_OVER)
+        requestAnimationFrame(loop);// calls the loop function as soon as browser is ready to render the changes
+}     
+
+loop();
